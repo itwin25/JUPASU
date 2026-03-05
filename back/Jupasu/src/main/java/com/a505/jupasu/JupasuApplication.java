ea914a6aad4f83678be77c379984f236d0b94a7e
박연준 <129067071+QUWR@users.noreply.github.com>
2026-03-05 11:34:05 +0900

    chore: init Spring Boot project
    스프링 프로젝트 설정

---
+++ back/Jupasu/src/main/java/com/a505/jupasu/JupasuApplication.java
@@ -0,0 +1,13 @@
+package com.a505.jupasu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JupasuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JupasuApplication.class, args);
+    }
+
+}
