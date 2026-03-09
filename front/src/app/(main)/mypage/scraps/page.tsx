5763326d9973ae5f570b1d30d2a478ec51b6a9a2
RASIT <shjh0815@naver.com>
2026-03-09 11:57:14 +0900

    fix : 문자열 깨짐 현상 수정

--- front/src/app/(main)/mypage/scraps/page.tsx
+++ front/src/app/(main)/mypage/scraps/page.tsx
@@ -1,1 +1,8 @@
-export default function Page() { return <div className='p-8'><h1>Mypage scraps Page</h1><p>¸¶ÀÌÆäÀÌÁö scraps °ü·Ã È­¸éÀÔ´Ï´Ù.</p></div>; }
+export default function Page() {
+  return (
+    <div className="p-8">
+      <h1>Mypage scraps Page</h1>
+      <p>ë§ˆì´í˜ì´ì§€ scraps ê´€ë ¨ í™”ë©´ì…ë‹ˆë‹¤.</p>
+    </div>
+  );
+}
