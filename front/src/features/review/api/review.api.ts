7974144ce6a2485b1bd2b6fdc0fc9d3fd8276142
정민선 <wjdalstjszzz@naver.com>
2026-03-17 13:14:23 +0900

    feat : 마이페이지 리뷰 조회 및 수정 삭제 API 연동

--- front/src/features/review/api/review.api.ts
+++ front/src/features/review/api/review.api.ts
@@ -1,6 +1,12 @@
 import { api } from '@/lib/axios';
 import { API_PATH } from '@/constants/api-path';
-import { CreateReviewRequest, MyPageReview, Review } from '../types/review.types';
+import {
+  CreateReviewRequest,
+  DeleteReviewRequest,
+  MyPageReview,
+  Review,
+  UpdateReviewRequest,
+} from '../types/review.types';
 import { ApiResponse, PaginatedResponse } from '@/types/api.types';
 
 export const reviewApi = {
@@ -8,15 +14,16 @@ export const reviewApi = {
     api.get<ApiResponse<MyPageReview[]>>(API_PATH.USER.REVIEWS).then((res) => res.data.data),
 
   getWineReviews: (wineId: string | number) =>
-    api
-      .get<PaginatedResponse<Review>>(API_PATH.REVIEW.LIST, { params: { wineId } })
-      .then((res) => res.data),
+    api.get<PaginatedResponse<Review>>(API_PATH.REVIEW.LIST(wineId)).then((res) => res.data),
 
   create: (data: CreateReviewRequest) =>
-    api.post(API_PATH.REVIEW.CREATE, data).then((res) => res.data),
+    api.post(API_PATH.REVIEW.CREATE(data.wineId), data).then((res) => res.data),
 
-  update: (id: string | number, content: string) =>
-    api.patch(API_PATH.REVIEW.DETAIL(id), { content }).then((res) => res.data),
+  update: ({ wineId, reviewId, rating, content }: UpdateReviewRequest) =>
+    api
+      .patch(API_PATH.REVIEW.DETAIL(wineId, reviewId), { rating, content })
+      .then((res) => res.data),
 
-  delete: (id: string | number) => api.delete(API_PATH.REVIEW.DETAIL(id)).then((res) => res.data),
+  delete: ({ wineId, reviewId }: DeleteReviewRequest) =>
+    api.delete(API_PATH.REVIEW.DETAIL(wineId, reviewId)).then((res) => res.data),
 };
