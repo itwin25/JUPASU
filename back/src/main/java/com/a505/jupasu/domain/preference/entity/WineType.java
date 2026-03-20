cbf2489b97fa8f91acd5adb3f8446c88195bfae3
이수인 <toto0887@gmail.com>
2026-03-20 17:42:55 +0900

    feat: preference 와인 타입 추가 (#S14P21A505-167)

--- back/src/main/java/com/a505/jupasu/domain/preference/entity/WineType.java
+++ back/src/main/java/com/a505/jupasu/domain/preference/entity/WineType.java
@@ -12,7 +12,9 @@ public enum WineType {
     RED("레드"),
     WHITE("화이트"),
     ROSE("로제"),
-    SPARKLING("스파클링");
+    SPARKLING("스파클링"),
+    DESSERT("디저트"),
+    FORTIFIED("주정강화");;
 
     private final String description;
 }
